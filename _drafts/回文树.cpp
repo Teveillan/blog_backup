5e5ebd2e29b54d1e537481741b21ac87c02ad6c6
Teveillan <teveillan@gmail.com>
2016-04-04 22:25:07 +0800

    update

--- _drafts/回文树.cpp
+++ _drafts/回文树.cpp
@@ -1,8 +1,7 @@
+const int LEN = 1e4 + 10;
 struct pt {
-	const int LEN = 1e4 + 100;
     int num[LEN], len[LEN], cnt[LEN], ch[LEN][26], fail[LEN], S[LEN];
-    int tot, len, last;
-
+    int tot, n, last;
     int creat(int l) {
         memset(ch[tot], 0, sizeof ch[tot]);
         cnt[tot] = num[tot] = 0;
@@ -10,18 +9,20 @@ struct pt {
         return tot++;
     }
     void init() {
-        tot = 0, last = 0, len = 0, S[len] = -1, fail[0] = 1;
+        tot = last = n = 0;
         creat(0);
         creat(-1);
+        S[n] = -1;
+        fail[0] = 1;
     }
     int get_fail(int x) {
-        while(S[len-len[x]-1] != S[len]) {
+        while(S[n-len[x]-1] != S[n]) {
             x = fail[x];
         }
         return x;
     }
     void add(int c) {
-        S[++len] = c;
+        S[++n] = c;
         int cur = get_fail(last);
         if(ch[cur][c] == 0) {
             int now = creat(len[cur] + 2);
